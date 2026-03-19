85e4291152d7746ebd6178250a23c8cdbcc8f292
water-eagle <130212192+water-eagle@users.noreply.github.com>
2026-03-19 19:11:59 +0900

    3주차 - 스프링 웹 MVC의 계층적 구조 - 도서 쇼핑몰 시작페이지 만들기

--- book-store/src/main/java/com/springmvc/controller/HomeController.java
+++ book-store/src/main/java/com/springmvc/controller/HomeController.java
@@ -1,15 +1,28 @@
 package com.springmvc.controller;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/", method=RequestMethod.GET)
-	public String home() {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		Date date = new Date();
+		DateFormat dataFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String formmattedDate = dataFormat.format(date);
+		model.addAttribute("serverTime", formmattedDate);
+
 		return "home";
 	}
+	// public String home() {
+	// return "home";
+	// }
 
 }
